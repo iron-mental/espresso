6f1442bcd630c723b9ce630044b10f2606d74741
josh <josh@watcha.com>
2020-09-02 22:22:05 +0900

    fix(...) : Main -> Home 으로 네이밍 변경, 홈화면 구성 추가

--- app/src/main/java/com/iron/espresso/HomeActivity.kt
+++ app/src/main/java/com/iron/espresso/HomeActivity.kt
@@ -1,11 +1,11 @@
 package com.iron.espresso
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
