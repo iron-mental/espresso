ed2a7f058f378a54b1d85651ddbe420dc9356a08
josh <josh@watcha.com>
2020-09-02 22:44:14 +0900

    feat(...) : 홈 3개 탭 fragment 만듬

---
+++ app/src/main/java/com/iron/espresso/SettingFragment.kt
@@ -0,0 +1,34 @@
+package com.iron.espresso
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.iron.espresso.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() =
+            SettingFragment()
+    }
+
+}
---
+++ app/src/main/java/com/iron/espresso/StudyFragment.kt
@@ -0,0 +1,35 @@
+package com.iron.espresso
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.iron.espresso.databinding.FragmentStudyBinding
+
+class StudyFragment : Fragment() {
+    private lateinit var binding: FragmentStudyBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_study, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() =
+            StudyFragment()
+    }
+}
+
+
+
