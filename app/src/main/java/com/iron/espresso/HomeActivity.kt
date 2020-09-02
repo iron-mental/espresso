5e4a3883f4c9c910755f8877af5f87b63d331bc1
josh <josh@watcha.com>
2020-09-02 22:43:51 +0900

    feat(...) : HomeActivity tab 3개 만들기

--- app/src/main/java/com/iron/espresso/HomeActivity.kt
+++ app/src/main/java/com/iron/espresso/HomeActivity.kt
@@ -2,10 +2,42 @@ package com.iron.espresso
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.iron.espresso.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        binding.lifecycleOwner = this
+
+        val homeTabList = listOf("스터디", "내 스터디", "설정")
+
+
+        binding.homePager.adapter = object : FragmentStatePagerAdapter(
+            supportFragmentManager,
+            BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+        ) {
+            override fun getCount(): Int =
+                homeTabList.size
+
+            override fun getItem(position: Int): Fragment =
+                when (position) {
+                    0 -> StudyFragment.newInstance()
+                    1 -> MyStudyFragment.newInstance()
+                    2 -> SettingFragment.newInstance()
+                    else -> error("Invalid position")
+                }
+
+            override fun getPageTitle(position: Int): CharSequence? =
+                homeTabList[position]
+        }
+
+        binding.bottomTab.setupWithViewPager(binding.homePager)
     }
 }
