aa6dbc4b56e97b7f44cea451f16a59ee3ecb66a9
Parkduksung <duksung1234@naver.com>
2020-09-10 02:47:08 +0900

    feat : BaseFragment 생성, 로그인/회원가입 관련한 Fragment/layout 생성, 버튼 클릭시 이벤트 처리(각 화면으로 이동)

--- app/src/main/java/com/iron/espresso/presentation/sign/IntroViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/sign/IntroViewModel.kt
@@ -1,8 +1,27 @@
 package com.iron.espresso.presentation.sign
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class IntroViewModel : ViewModel() {
 
+    private val _clickTypeIdentifier =
+        MutableLiveData<String>()
+    val clickTypeIdentifier
+        get() = _clickTypeIdentifier
 
+
+    fun startSignUp() {
+        _clickTypeIdentifier.value = TYPE_SIGN_UP
+    }
+
+    fun startSignIn() {
+        _clickTypeIdentifier.value = TYPE_SIGN_IN
+    }
+
+    companion object {
+        const val TYPE_SIGN_UP = "signUp"
+        const val TYPE_SIGN_IN = "signIn"
+
+    }
 }
