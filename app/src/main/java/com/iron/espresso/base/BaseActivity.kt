c3fc9e82bfb4b96ff360d9824772fa5e43914e80
Parkduksung <duksung1234@naver.com>
2020-09-10 01:10:45 +0900

    fix : DataBindingActivity -> BaseActivity 수정

---
+++ app/src/main/java/com/iron/espresso/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.iron.espresso.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
+    AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
+}
--- app/src/main/java/com/iron/espresso/presentation/sign/IntroViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/sign/IntroViewModel.kt
@@ -4,4 +4,5 @@ import androidx.lifecycle.ViewModel
 
 class IntroViewModel : ViewModel() {
 
+
 }
