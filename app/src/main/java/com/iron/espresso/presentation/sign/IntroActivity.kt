0cf3985c98f654533338322cf0ad0d00df31268f
Parkduksung <duksung1234@naver.com>
2020-09-10 01:10:45 +0900

    fix : DataBindingActivity -> BaseActivity 수정

--- app/src/main/java/com/iron/espresso/presentation/sign/IntroActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/sign/IntroActivity.kt
@@ -2,13 +2,12 @@ package com.iron.espresso.presentation.sign
 
 import android.os.Bundle
 import com.iron.espresso.R
-import com.iron.espresso.base.DataBindingActivity
+import com.iron.espresso.base.BaseActivity
 import com.iron.espresso.databinding.ActivityIntroBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class IntroActivity : DataBindingActivity() {
+class IntroActivity : BaseActivity<ActivityIntroBinding>(R.layout.activity_intro) {
 
-    private val binding: ActivityIntroBinding by binding(R.layout.activity_intro)
     private val introViewModel by viewModel<IntroViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,4 +18,5 @@ class IntroActivity : DataBindingActivity() {
             vm = introViewModel
         }
     }
+
 }
