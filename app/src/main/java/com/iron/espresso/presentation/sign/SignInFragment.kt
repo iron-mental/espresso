4d33e5c533f1d61c9289ab5d44524768a04dee34
Parkduksung <duksung1234@naver.com>
2020-09-13 01:56:32 +0900

    feat : SignInViewModel 생성 , sign_in layout 구상.

--- app/src/main/java/com/iron/espresso/presentation/sign/SignInFragment.kt
+++ app/src/main/java/com/iron/espresso/presentation/sign/SignInFragment.kt
@@ -5,9 +5,17 @@ import android.view.View
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseFragment
 import com.iron.espresso.databinding.FragmentSignInBinding
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
+
+    private val signInViewModel by sharedViewModel<SignInViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.run {
+            vm = signInViewModel
+        }
     }
 }
---
+++ app/src/main/java/com/iron/espresso/presentation/sign/SignInViewModel.kt
@@ -0,0 +1,6 @@
+package com.iron.espresso.presentation.sign
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+}
