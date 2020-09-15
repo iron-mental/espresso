4ab90220b5e63d067e4f65038601b737d6e88a2d
josh <josh@watcha.com>
2020-09-15 23:34:04 +0900

    chore(...) : string res 텍스트 변경

--- app/src/main/java/com/iron/espresso/MenuSet.kt
+++ app/src/main/java/com/iron/espresso/MenuSet.kt
@@ -5,5 +5,5 @@ import androidx.annotation.IdRes
 import androidx.annotation.StringRes
 
 enum class MenuSet(@DrawableRes val imageResId: Int, @IdRes val menuId: Int, @StringRes val titleResId: Int) {
-    ICON_SHARE(R.drawable.ic_share_24, R.id.menu_item_share, R.string.more)
+    ICON_SHARE(R.drawable.ic_share_24, R.id.menu_item_share, R.string.share)
 }
