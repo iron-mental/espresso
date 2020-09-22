f39bddf92c89db5a37385df3300cfb2406ddefb8
dspark <dspark@rsupport.com>
2020-09-22 20:21:30 +0900

    feat(SignInActivity) : SignInActivity , activity_sign_in 생성.

---
+++ app/src/main/java/com/iron/espresso/presentation/sign/SignInActivity.kt
@@ -0,0 +1,10 @@
+package com.iron.espresso.presentation.sign
+
+import com.iron.espresso.R
+import com.iron.espresso.base.BaseActivity
+import com.iron.espresso.databinding.ActivitySignInBinding
+
+class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+
+
+}
