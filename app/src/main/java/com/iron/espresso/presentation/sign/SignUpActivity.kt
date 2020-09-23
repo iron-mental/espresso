101164d6ac0f56c07f4fe6f99c6e35e4eb838ae4
dspark <dspark@rsupport.com>
2020-09-23 23:25:30 +0900

    feat(SignUpActivity,activity_sign_up) : 생성.

---
+++ app/src/main/java/com/iron/espresso/presentation/sign/SignUpActivity.kt
@@ -0,0 +1,9 @@
+package com.iron.espresso.presentation.sign
+
+import com.iron.espresso.R
+import com.iron.espresso.base.BaseActivity
+import com.iron.espresso.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+
+}
