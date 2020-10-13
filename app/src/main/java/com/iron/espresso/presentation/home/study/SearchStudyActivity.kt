feda8a42ce4619f85b407bfa26998ffcf7c90b2a
YoungChan <kychan1320@gmail.com>
2020-10-13 17:13:35 +0900

    chore(...) : 스터디 검색 화면 추가

---
+++ app/src/main/java/com/iron/espresso/presentation/home/study/SearchStudyActivity.kt
@@ -0,0 +1,13 @@
+package com.iron.espresso.presentation.home.study
+
+import android.os.Bundle
+import com.iron.espresso.R
+import com.iron.espresso.base.BaseActivity
+import com.iron.espresso.databinding.ActivitySearchStudyBinding
+
+class SearchStudyActivity : BaseActivity<ActivitySearchStudyBinding>(R.layout.activity_search_study) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
