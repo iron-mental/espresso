370f9f961426bd1fc6b089803a14af263a03cd14
YoungChan <kychan1320@gmail.com>
2020-10-15 14:24:53 +0900

    chore(...) : 버튼 정적 생성 및 클릭대응

--- app/src/main/java/com/iron/espresso/presentation/home/study/SearchStudyActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/SearchStudyActivity.kt
@@ -1,13 +1,18 @@
 package com.iron.espresso.presentation.home.study
 
 import android.os.Bundle
+import android.view.View
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseActivity
 import com.iron.espresso.databinding.ActivitySearchStudyBinding
+import kotlinx.android.synthetic.main.activity_search_study.*
 
-class SearchStudyActivity : BaseActivity<ActivitySearchStudyBinding>(R.layout.activity_search_study) {
+class SearchStudyActivity : BaseActivity<ActivitySearchStudyBinding>(R.layout.activity_search_study),View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onClick(p0: View?) {
     }
 }
