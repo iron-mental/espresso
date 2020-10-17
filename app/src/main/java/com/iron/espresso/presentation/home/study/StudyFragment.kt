ef9131174dae9b960da04746aaabc2d9c4248b36
Parkduksung <duksung1234@naver.com>
2020-10-17 18:19:25 +0900

    feat(StudyFragment) : toolbarHelper 를 이용한 toolbar 적용.

--- app/src/main/java/com/iron/espresso/presentation/home/study/StudyFragment.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/StudyFragment.kt
@@ -1,22 +1,25 @@
 package com.iron.espresso.presentation.home.study
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.iron.espresso.R
 import com.iron.espresso.databinding.FragmentStudyBinding
+import com.iron.espresso.utils.ToolbarHelper
 
 class StudyFragment : Fragment() {
     private lateinit var binding: FragmentStudyBinding
+
+    private lateinit var toolbarHelper: ToolbarHelper
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_study, container, false)
         binding.lifecycleOwner = this
         return binding.root
@@ -24,9 +27,35 @@ class StudyFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        toolbarHelper = ToolbarHelper((activity as AppCompatActivity), binding.appbar).apply {
+            setTitle(TOOLBAR_TITLE)
+        }
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_study, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search_study -> {
+            }
+
+            R.id.add_study -> {
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
     companion object {
+
+        private const val TOOLBAR_TITLE = "스터디"
+
         fun newInstance() =
             StudyFragment()
     }
