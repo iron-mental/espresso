cbccfc2defc59c7d29a78c6a73077f64f20019a8
josh <josh@watcha.com>
2020-10-18 15:56:10 +0900

    fix(...) : 다크테마 적용 변경

--- app/src/main/java/com/iron/espresso/App.kt
+++ app/src/main/java/com/iron/espresso/App.kt
@@ -1,8 +1,6 @@
 package com.iron.espresso
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,11 +8,5 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
-//        startKoin {
-//            androidContext(this@App)
-//            this.modules(viewModelModule)
-//        }
     }
 }
