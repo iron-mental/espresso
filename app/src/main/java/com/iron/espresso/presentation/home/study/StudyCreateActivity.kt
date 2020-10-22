aa152b591cbb7eb8c2b49e052aebc09d53e7cd6d
Parkduksung <duksung1234@naver.com>
2020-10-23 01:29:30 +0900

    feat(StudyCreateActivity) : Toolbar 적용한 스터디 만들기 기본화면 생성.

--- app/src/main/java/com/iron/espresso/presentation/home/study/StudyCreateActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/StudyCreateActivity.kt
@@ -3,14 +3,18 @@ package com.iron.espresso.presentation.home.study
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.iron.espresso.R
 import com.iron.espresso.databinding.ActivityCreateStudyBinding
+import com.iron.espresso.utils.ToolbarHelper
 
 class StudyCreateActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCreateStudyBinding
+
+    private lateinit var toolbarHelper: ToolbarHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +22,29 @@ class StudyCreateActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_study)
         binding.lifecycleOwner = this
 
+        toolbarHelper = ToolbarHelper(this, binding.appbar).apply {
+            setTitle(TITLE)
+            setNavigationIcon(R.drawable.ic_back_24)
+        }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+            }
+        }
+        return true
     }
 
     companion object {
+
+        private const val TITLE = "스터디 만들기"
+
         fun getInstance(context: Context) =
             Intent(context, StudyCreateActivity::class.java)
     }
+
+
 }
