05303545c4e417b7856b4c5bb3fb759347d3c5da
ko0chan <kychan1320@gmail.com>
2020-10-29 23:32:26 +0900

    feat(StudyDetailActivity) : viewpager adapter 설정

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/StudyDetailActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/StudyDetailActivity.kt
@@ -2,14 +2,16 @@ package com.iron.espresso.presentation.home.mystudy
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.iron.espresso.R
 import com.iron.espresso.databinding.ActivityStudyDetailBinding
+import com.iron.espresso.presentation.home.mystudy.studydetail.ChattingFragment
 import com.iron.espresso.presentation.home.mystudy.studydetail.NoticeFragment
+import com.iron.espresso.presentation.home.mystudy.studydetail.StudyInfoFragment
 
 class StudyDetailActivity : AppCompatActivity() {
 
@@ -19,15 +21,25 @@ class StudyDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_study_detail)
 
-        val testButton = Button(this).apply {
-            text = "스터디 상세"
-            setOnClickListener {
-                binding.containerNotice.bringToFront()
-                supportFragmentManager.beginTransaction()
-                    .add(R.id.container_notice, NoticeFragment()).commit()
+        val studyDetailTabList = resources.getStringArray(R.array.study_detail_tab)
+
+        binding.run {
+            studyDetailPager.adapter = object : FragmentStateAdapter(
+                supportFragmentManager,
+                lifecycle
+            ) {
+                override fun getItemCount(): Int =
+                    studyDetailTabList.size
+
+                override fun createFragment(position: Int): Fragment =
+                    when (position) {
+                        0 -> NoticeFragment.newInstance()
+                        1 -> StudyInfoFragment.newInstance()
+                        2 -> ChattingFragment.newInstance()
+                        else -> error("Invalid position")
+                    }
             }
         }
-        (binding.root as ViewGroup).addView(testButton)
     }
 
     companion object {
