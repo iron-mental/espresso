ebdcdfd9e5a432f1074602dd99b74b5ccc614776
ko0chan <kychan1320@gmail.com>
2020-10-29 21:03:55 +0900

    chore(NoticeFragment) : 공지사항 어댑터 연결

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeFragment.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.iron.espresso.R
+import com.iron.espresso.data.model.NoticeListItem
 import com.iron.espresso.databinding.FragmentNoticeBinding
+import com.iron.espresso.presentation.home.mystudy.adapter.NoticeAdapter
 
 class NoticeFragment : Fragment() {
     private lateinit var binding: FragmentNoticeBinding
@@ -24,6 +26,18 @@ class NoticeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val noticeList = mutableListOf<NoticeListItem>().apply {
+            add(NoticeListItem("공지", "강남역 윙스터디", "강남구", "10/27"))
+            add(NoticeListItem("공지", "강남역 윙스터디", "강남구", "10/27"))
+            add(NoticeListItem("공지", "강남역 윙스터디", "강남구", "10/27"))
+            add(NoticeListItem("공지", "강남역 윙스터디", "강남구", "10/27"))
+            add(NoticeListItem("공지", "강남역 윙스터디", "강남구", "10/27"))
+            add(NoticeListItem("공지", "강남역 윙스터디", "강남구", "10/27"))
+            add(NoticeListItem("공지", "강남역 윙스터디", "강남구", "10/27"))
+        }
+
+        binding.noticeList.adapter = NoticeAdapter(noticeList)
     }
 
     companion object {
