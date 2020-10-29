d469b7481d9c3e540e7a6f40cc7852499c52d821
ko0chan <kychan1320@gmail.com>
2020-10-29 23:17:41 +0900

    chore(...) : 스터디 정보, 채팅 fragment 추가

---
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/StudyInfoFragment.kt
@@ -0,0 +1,33 @@
+package com.iron.espresso.presentation.home.mystudy.studydetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.iron.espresso.R
+import com.iron.espresso.databinding.FragmentStudyInfoBinding
+
+class StudyInfoFragment : Fragment() {
+    private lateinit var binding: FragmentStudyInfoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_study_info, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() =
+            StudyInfoFragment()
+    }
+}
