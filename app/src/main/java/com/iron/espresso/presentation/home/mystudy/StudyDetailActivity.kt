29ae81f4e27616f5ac3fed04b2719a8e018f2c3d
ko0chan <kychan1320@gmail.com>
2020-10-29 18:20:12 +0900

    chore(...) : 공지사항 화면 추가

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/StudyDetailActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/StudyDetailActivity.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import com.iron.espresso.R
 import com.iron.espresso.databinding.ActivityStudyDetailBinding
+import com.iron.espresso.presentation.home.mystudy.studydetail.NoticeFragment
 
 class StudyDetailActivity : AppCompatActivity() {
 
@@ -15,6 +18,16 @@ class StudyDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_study_detail)
+
+        val testButton = Button(this).apply {
+            text = "스터디 상세"
+            setOnClickListener {
+                binding.containerNotice.bringToFront()
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.container_notice, NoticeFragment()).commit()
+            }
+        }
+        (binding.root as ViewGroup).addView(testButton)
     }
 
     companion object {
---
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeFragment.kt
@@ -0,0 +1,36 @@
+package com.iron.espresso.presentation.home.mystudy.studydetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.iron.espresso.R
+import com.iron.espresso.databinding.FragmentNoticeBinding
+
+class NoticeFragment : Fragment() {
+    private lateinit var binding: FragmentNoticeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_notice, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() =
+            NoticeFragment()
+    }
+}
+
+
+
