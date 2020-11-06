067f5c59c8730eb69af420b6dcc539683e7f3d91
Parkduksung <duksung1234@naver.com>
2020-11-06 23:30:14 +0900

    chore(...) : 디자인부분 수정. 재플린 참조. ToolbarHelper 이슈 발견.

--- app/src/main/java/com/iron/espresso/ext/ImageViewExt.kt
+++ app/src/main/java/com/iron/espresso/ext/ImageViewExt.kt
@@ -3,16 +3,29 @@ package com.iron.espresso.ext
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.iron.espresso.R
 
-@BindingAdapter("bind:setUrlImg")
-fun ImageView.setUrlImg(url: String?) {
-    Glide.with(context)
-        .load(url)
-        .centerCrop()
-        .error(R.drawable.dummy_image)
-        .into(this)
+@BindingAdapter("bind:setUrlImg", "bind:type")
+fun ImageView.setUrlImg(url: String?, type: ImageType?) {
+    when (type) {
+        ImageType.NORMAl -> {
+            Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .error(R.drawable.dummy_image)
+                .into(this)
+        }
+        ImageType.ROUND -> {
+            Glide.with(context)
+                .load(R.drawable.android)
+                .apply(RequestOptions.circleCropTransform())
+                .error(R.drawable.dummy_image)
+                .into(this)
+        }
+    }
 }
+
 
 @BindingAdapter("bind:setStudyCategoryImg")
 fun ImageView.setStudyCategoryImg(drawable: Int?) {
@@ -21,4 +34,9 @@ fun ImageView.setStudyCategoryImg(drawable: Int?) {
         .fitCenter()
         .error(R.drawable.dummy_image)
         .into(this)
+}
+
+
+enum class ImageType {
+    ROUND, NORMAl
 }
