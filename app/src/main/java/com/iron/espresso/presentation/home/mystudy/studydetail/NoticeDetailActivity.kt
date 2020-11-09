d7daec6e8626bddb06f0768a86883706a33c0d57
ko0chan <kychan1320@gmail.com>
2020-11-10 02:08:17 +0900

    chore(NoticeDetailActivity) : API TEST 임시

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeDetailActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeDetailActivity.kt
@@ -4,13 +4,47 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
+import com.google.gson.annotations.SerializedName
 import com.iron.espresso.R
 import com.iron.espresso.ToolbarHelper
 import com.iron.espresso.data.model.NoticeItemType
 import com.iron.espresso.data.model.NoticeListItem
 import com.iron.espresso.databinding.ActivityNoticeDetailBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RetrofitNetwork {
+    @GET("/v1/study/{study_id}/notice/{notice_id}")
+    fun getTest(
+        @Path("study_id") studyId: Int,
+        @Path("notice_id") noticeId: Int
+    ): Call<NoticeResponse>
+}
+data class NoticeResponse(
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("study_id")
+    val studyId: Int?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("contents")
+    val contents: String?,
+    @SerializedName("pinned")
+    val pinned: Boolean?,
+    @SerializedName("created_at")
+    val createAt: String?,
+    @SerializedName("updated_at")
+    val updatedAt: String?
+)
+
 
 class NoticeDetailActivity : AppCompatActivity() {
 
@@ -39,6 +73,38 @@ class NoticeDetailActivity : AppCompatActivity() {
                 else -> error("error")
             }
         }
+
+        //retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://3.35.154.27:3000")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(RetrofitNetwork::class.java)
+        val callGetTest = service.getTest(1, 1)
+
+        callGetTest.enqueue(object : Callback<NoticeResponse> {
+            override fun onResponse(
+                call: Call<NoticeResponse>,
+                response: Response<NoticeResponse>
+            ) {
+                Log.d("TAG","response : ${response.body()!!.studyId}")
+                Log.d("TAG","response : ${response.errorBody()}")
+                Log.d("TAG","response : ${response.message()}")
+                Log.d("TAG","response : ${response.code()}")
+                Log.d("TAG", "성공 : ${response.raw()}")
+                if (response.isSuccessful) {
+                    Log.d("Response :: ", "${response.body()}")
+                } else {
+                    Log.d("Response :: ", "실패")
+                }
+                val data: NoticeResponse? = response.body()
+            }
+
+            override fun onFailure(call: Call<NoticeResponse>, t: Throwable) {
+                Log.d("TAG", "실패 : $t")
+            }
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
