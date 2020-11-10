f910ec3018604e3c8f4994fc3f76f3707f884a65
ko0chan <kychan1320@gmail.com>
2020-11-10 21:42:26 +0900

    chore(SearchPlaceActivity) : 툴바 EditText custom 추가

--- app/src/main/java/com/iron/espresso/presentation/home/study/SearchPlaceActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/SearchPlaceActivity.kt
@@ -2,10 +2,17 @@ package com.iron.espresso.presentation.home.study
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.marginEnd
 import androidx.databinding.DataBindingUtil
+import com.iron.espresso.MenuSet
 import com.iron.espresso.R
 import com.iron.espresso.ToolbarHelper
 import com.iron.espresso.databinding.ActivitySearchPlaceBinding
@@ -14,14 +21,25 @@ class SearchPlaceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchPlaceBinding
     private lateinit var toolbarHelper: ToolbarHelper
+    private lateinit var searchEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search_place)
 
-        toolbarHelper = ToolbarHelper(this, binding.appbar).apply {
+        searchEditText = EditText(this)
+        toolbarHelper = ToolbarHelper(this, binding.appbar.apply {
+        }).apply {
             setTitle(null)
             setNavigationIcon(R.drawable.ic_back_24)
+            setCustomView(searchEditText.apply {
+                hint = TOOLBAR_HINT
+                setSingleLine()
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            })
         }
     }
 
@@ -37,5 +55,7 @@ class SearchPlaceActivity : AppCompatActivity() {
     companion object {
         fun getInstance(context: Context) =
             Intent(context, SearchPlaceActivity::class.java)
+
+        const val TOOLBAR_HINT = "넥슨"
     }
 }
