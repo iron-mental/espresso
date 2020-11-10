24e3872d00f53c54be3b7403bcc94b7ea8f814d5
ko0chan <kychan1320@gmail.com>
2020-11-10 20:17:03 +0900

    chore(...) : 앱바 초기설정

--- app/src/main/java/com/iron/espresso/presentation/home/study/SearchPlaceActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/SearchPlaceActivity.kt
@@ -4,17 +4,34 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import com.iron.espresso.R
+import com.iron.espresso.ToolbarHelper
 import com.iron.espresso.databinding.ActivitySearchPlaceBinding
 
 class SearchPlaceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchPlaceBinding
+    private lateinit var toolbarHelper: ToolbarHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search_place)
+
+        toolbarHelper = ToolbarHelper(this, binding.appbar).apply {
+            setTitle(null)
+            setNavigationIcon(R.drawable.ic_back_24)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+            }
+        }
+        return true
     }
 
     companion object {
