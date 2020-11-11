5e0b6f07e5169df1018b1601d8524d6679ad5e84
ko0chan <kychan1320@gmail.com>
2020-11-11 15:30:03 +0900

    chore(...) : 장소목록아이템 데이터 설정

--- app/src/main/java/com/iron/espresso/presentation/home/study/adapter/PlaceAdapter.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/adapter/PlaceAdapter.kt
@@ -3,17 +3,23 @@ package com.iron.espresso.presentation.home.study.adapter
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.iron.espresso.data.model.PlaceItem
+import com.iron.espresso.presentation.home.study.adapter.viewholder.PlaceViewHolder
 
-class PlaceAdapter : RecyclerView.Adapter<>() {
+class PlaceAdapter : RecyclerView.Adapter<PlaceViewHolder>() {
 
     private val placeList = mutableListOf<PlaceItem>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    fun setItemList(placeList: List<PlaceItem>) {
+        this.placeList.clear()
+        this.placeList.addAll(placeList)
+        notifyDataSetChanged()
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder =
+        PlaceViewHolder(parent)
+
+    override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+        holder.bind(placeList[position])
     }
 
     override fun getItemCount(): Int =
--- app/src/main/java/com/iron/espresso/presentation/home/study/adapter/viewholder/PlaceViewHolder.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/adapter/viewholder/PlaceViewHolder.kt
@@ -5,14 +5,20 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.iron.espresso.R
+import com.iron.espresso.data.model.PlaceItem
 
 class PlaceViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.item_place_list, parent, false)
 ) {
     private val binding =
-        DataBindingUtil.bind<com.iron.espresso.databinding.ItemStudyCategoryBinding>(itemView)
+        DataBindingUtil.bind<com.iron.espresso.databinding.ItemPlaceListBinding>(itemView)
 
-    fun bind(){
+    fun bind(item: PlaceItem) {
+        binding?.run {
+            title.text = item.title
+            category.text = item.category
+            address.text = item.address
+        }
 
     }
 }
