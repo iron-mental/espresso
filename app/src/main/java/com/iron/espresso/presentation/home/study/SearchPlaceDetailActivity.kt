39549a2044178cd7dd1f85fa5b233632a2604bb5
ko0chan <kychan1320@gmail.com>
2020-11-12 17:13:57 +0900

    chore(...) : 장소상세검색화면 시작

---
+++ app/src/main/java/com/iron/espresso/presentation/home/study/SearchPlaceDetailActivity.kt
@@ -0,0 +1,17 @@
+package com.iron.espresso.presentation.home.study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.iron.espresso.R
+import com.iron.espresso.databinding.ActivitySearchPlaceDetailBinding
+
+class SearchPlaceDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchPlaceDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search_place_detail)
+    }
+}
