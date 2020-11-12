5be54e6797ad4646f5cdc3661ca989472a143b8e
ko0chan <kychan1320@gmail.com>
2020-11-12 16:44:56 +0900

    chore(PlaceResponse) : api Response model 구성 임시구조

--- app/src/main/java/com/iron/espresso/model/api/KakaoApi.kt
+++ app/src/main/java/com/iron/espresso/model/api/KakaoApi.kt
@@ -1,14 +1,16 @@
 package com.iron.espresso.model.api
 
-import com.iron.espresso.presentation.home.study.PlaceResponse
+import com.iron.espresso.model.response.PlaceResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface KakaoApi {
+
+    //키워드로 장소 검색
     @GET("v2/local/search/keyword.json")
-    fun getTest(
+    fun getPlacesByKeyword(
         @Header("Authorization") authorization: String,
         @Query("query") query: String
     ): Call<PlaceResponse>
---
+++ app/src/main/java/com/iron/espresso/model/response/PlaceResponse.kt
@@ -0,0 +1,57 @@
+package com.iron.espresso.model.response
+
+import com.google.gson.annotations.SerializedName
+
+data class PlaceResponse(
+    @SerializedName("meta")
+    val meta: PlaceMeta,
+    @SerializedName("documents")
+    val documents: List<Place>
+)
+
+data class PlaceMeta(
+    @SerializedName("total_count")
+    val totalCount: Int,
+    @SerializedName("pageable_count")
+    val pageableCount: Int,
+    @SerializedName("is_end")
+    val isEnd: Boolean,
+    @SerializedName("same_name")
+    val sameName: RegionInfo
+)
+
+data class RegionInfo(
+    @SerializedName("region")
+    val region: List<String>,
+    @SerializedName("keyword")
+    val keyword: String,
+    @SerializedName("selected_region")
+    val selected_region: String
+)
+
+data class Place(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("place_name")
+    val place_name: String,
+    @SerializedName("category_name")
+    val categoryName: String,
+    @SerializedName("category_group_code")
+    val categoryGroupCode: String,
+    @SerializedName("category_group_name")
+    val categoryGroupName: String,
+    @SerializedName("phone")
+    val phone: String,
+    @SerializedName("address_name")
+    val addressName: String,
+    @SerializedName("road_address_name")
+    val roadAddressName: String,
+    @SerializedName("x")
+    val x: String,
+    @SerializedName("y")
+    val y: String,
+    @SerializedName("place_url")
+    val place_url: String,
+    @SerializedName("distance")
+    val distance: String
+)
