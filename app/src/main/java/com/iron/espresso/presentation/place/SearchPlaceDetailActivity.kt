baf951ae8ce0869b1ae4af5a10063d3a558c2b82
ko0chan <kychan1320@gmail.com>
2020-11-16 14:49:41 +0900

    feat(SearchPlaceDetailActivity) : retrofit KaKaoApi 통신 및 값 설정

--- app/src/main/java/com/iron/espresso/presentation/place/SearchPlaceDetailActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/place/SearchPlaceDetailActivity.kt
@@ -3,18 +3,24 @@ package com.iron.espresso.presentation.place
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import androidx.annotation.UiThread
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import com.iron.espresso.R
 import com.iron.espresso.databinding.ActivitySearchPlaceDetailBinding
-import com.iron.espresso.model.response.Place
+import com.iron.espresso.model.api.KakaoApi
+import com.iron.espresso.model.response.*
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchPlaceDetailActivity : FragmentActivity(), OnMapReadyCallback {
 
@@ -48,15 +54,52 @@ class SearchPlaceDetailActivity : FragmentActivity(), OnMapReadyCallback {
 
         //카메라 이동 멈췄을 때
         naverMap.addOnCameraIdleListener {
-            binding.address.text = when {
-                placeItems.roadAddressName.isEmpty() -> {
-                    placeItems.addressName
-                }
-                else -> {
-                    placeItems.roadAddressName
-                }
-            }
+
+            searchCoord(
+                naverMap.cameraPosition.target.longitude,
+                naverMap.cameraPosition.target.latitude
+            )
         }
+    }
+
+    private fun searchCoord(lat: Double, lng: Double) {
+
+        //retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl(SearchPlaceActivity.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(KakaoApi::class.java)
+        val callGetLocal = service.convertAddressToCoord(SearchPlaceActivity.REST_API_KEY, lat, lng)
+
+        callGetLocal.enqueue(object : Callback<LocalResponse> {
+            override fun onResponse(
+                call: Call<LocalResponse>,
+                response: Response<LocalResponse>
+            ) {
+                Log.d("TAG", "response : ${response.body()?.documents}")
+                Log.d("TAG", "성공 : ${response.raw()}")
+
+                val localList = response.body()?.documents
+                binding.address.text = localList?.get(0)?.address?.addressName
+
+                binding.address.text = when {
+                    localList?.get(0)?.roadAddress?.addressName?.isEmpty()!! -> {
+                        localList[0].address.addressName
+                    }
+                    else -> {
+                        localList[0].roadAddress.addressName
+                    }
+                }
+
+            }
+
+            override fun onFailure(call: Call<LocalResponse>, t: Throwable) {
+                Log.d("TAG", "실패 : $t")
+            }
+        })
+
 
     }
 
