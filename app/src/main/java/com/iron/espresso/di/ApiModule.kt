ba2759bccf617b1e786f832abb0ffe409f2e6e5a
josh <josh@watcha.com>
2020-11-18 02:40:17 +0900

    feat(...) : 통신 logging client

--- app/src/main/java/com/iron/espresso/di/ApiModule.kt
+++ app/src/main/java/com/iron/espresso/di/ApiModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -33,10 +34,15 @@ object ApiModule {
     @Singleton
     @Provides
     fun provideProjectApi(): ProjectApi {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         return Retrofit.Builder()
             .baseUrl(API_URL)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(ProjectApi::class.java)
     }
