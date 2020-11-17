d4e56068a81a9535395a98ebb7b4c4deea478509
josh <josh@watcha.com>
2020-11-18 00:55:07 +0900

    fix(BaseResponse) : type, label 추가

--- app/src/main/java/com/iron/espresso/model/response/BaseResponse.kt
+++ app/src/main/java/com/iron/espresso/model/response/BaseResponse.kt
@@ -3,7 +3,9 @@ package com.iron.espresso.model.response
 import com.google.gson.annotations.SerializedName
 
 data class BaseResponse<T>(
-    @SerializedName("result") val result: Boolean?,
+    @SerializedName("result") val result: Boolean,
+    @SerializedName("type") val type: String?,
+    @SerializedName("label") val label: String?,
     @SerializedName("message") val message: String?,
     @SerializedName("data") val data: T?,
 )
