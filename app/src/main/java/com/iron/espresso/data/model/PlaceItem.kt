2105b62d5be4061b6ff9ed57a8be3ca98861c393
ko0chan <kychan1320@gmail.com>
2020-11-17 18:12:35 +0900

    chore(PlaceItem) : place model 수정

--- app/src/main/java/com/iron/espresso/data/model/PlaceItem.kt
+++ app/src/main/java/com/iron/espresso/data/model/PlaceItem.kt
@@ -1,7 +1,13 @@
 package com.iron.espresso.data.model
 
-data class PlaceItem (
-    val title: String,
-    val category: String,
-    val address: String
-)
+import java.io.Serializable
+
+data class PlaceItem(
+    val lat: Double,
+    val lng: Double,
+    val sido: String,
+    val sigungu: String,
+    val addressName: String,
+    val placeName: String?,
+    val locationDetail: String?
+): Serializable
