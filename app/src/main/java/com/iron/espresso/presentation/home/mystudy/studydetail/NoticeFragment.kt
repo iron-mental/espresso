2956bf49e492d95318f2b29acaea92be1a17e4a3
ko0chan <kychan1320@gmail.com>
2020-12-02 23:40:04 +0900

    feat(...) : viewModel 적용 및 공지사항 목록 조회 API 통신

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeFragment.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeFragment.kt
@@ -3,47 +3,44 @@ package com.iron.espresso.presentation.home.mystudy.studydetail
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseFragment
-import com.iron.espresso.data.model.NoticeItemType
-import com.iron.espresso.data.model.NoticeListItem
 import com.iron.espresso.databinding.FragmentNoticeBinding
+import com.iron.espresso.model.response.notice.NoticeListResponse
+import com.iron.espresso.model.response.notice.NoticeResponse
+import com.iron.espresso.presentation.home.mystudy.StudyDetailActivity.Companion.DEFAULT_VALUE
 import com.iron.espresso.presentation.home.mystudy.StudyDetailActivity.Companion.STUDY_ID
 import com.iron.espresso.presentation.home.mystudy.adapter.NoticeAdapter
 
 class NoticeFragment : BaseFragment<FragmentNoticeBinding>(R.layout.fragment_notice) {
 
     private val noticeAdapter = NoticeAdapter()
+    private lateinit var noticeList: NoticeListResponse
+
+    private val viewModel by lazy {
+        requireActivity().viewModels<NoticeViewModel>()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val noticeListItem = mutableListOf<NoticeListItem>().apply {
-            add(NoticeListItem("강남역 윙스터디1", "강\n남\n구\n청", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디2", "강남구", "10/27", NoticeItemType.HEADER))
-            add(NoticeListItem("강남역 윙스터디3", "강남구", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디4", "강남구", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디5", "강남구", "10/27", NoticeItemType.HEADER))
-            add(NoticeListItem("강남역 윙스터디6", "강남구", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디7", "강남구", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디8", "강\n남\n구\n청", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디9", "강남구", "10/27", NoticeItemType.HEADER))
-            add(NoticeListItem("강남역 윙스터디10", "강남구", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디11", "강남구", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디12", "강남구", "10/27", NoticeItemType.HEADER))
-            add(NoticeListItem("강남역 윙스터디13", "강남구", "10/27", NoticeItemType.ITEM))
-            add(NoticeListItem("강남역 윙스터디14", "강남구", "10/27", NoticeItemType.ITEM))
-        }
+        val studyId = arguments?.getInt(STUDY_ID) ?: DEFAULT_VALUE
 
-        noticeListItem.sortBy { it.type }
+        viewModel.value.showNoticeList(studyId)
 
-        noticeAdapter.run {
-            setItemList(noticeListItem)
-            setItemClickListener { noticeItem : NoticeListItem ->
-                Toast.makeText(context, "onClick position: $noticeItem", Toast.LENGTH_SHORT).show()
-                startActivity(context?.let { NoticeDetailActivity.getInstance(it) })
+        viewModel.value.noticeListItem.observe(viewLifecycleOwner, { noticeListItem ->
+            noticeList = noticeListItem
+            noticeAdapter.run {
+                setItemList(noticeList)
             }
+        })
+
+        noticeAdapter.setItemClickListener { noticeItem: NoticeResponse ->
+            Toast.makeText(context, "onClick position: $noticeItem", Toast.LENGTH_SHORT).show()
+            startActivity(context?.let { NoticeDetailActivity.getInstance(it) })
         }
+
         binding.noticeList.adapter = noticeAdapter
 
     }
---
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeViewModel.kt
@@ -0,0 +1,40 @@
+package com.iron.espresso.presentation.home.mystudy.studydetail
+
+import android.annotation.SuppressLint
+import androidx.lifecycle.MutableLiveData
+import com.iron.espresso.base.BaseViewModel
+import com.iron.espresso.di.ApiModule
+import com.iron.espresso.ext.networkSchedulers
+import com.iron.espresso.model.response.notice.NoticeListResponse
+
+class NoticeViewModel : BaseViewModel() {
+
+    private val token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksImVtYWlsIjoicmhkdWRja3NAbmF2ZXIuY29tIiwibmlja25hbWUiOiLqs6DsmIHssKwiLCJpYXQiOjE2MDU4NDk0MzMsImV4cCI6MTYwNzE0NTQzMywiaXNzIjoidGVybWluYWwtc2VydmVyIiwic3ViIjoidXNlckluZm8tYWNjZXNzIn0.Eptf9T9Z_c-VmIUqLNV5CKAN-ftm1sZSwOzs91SrIr0"
+
+    val noticeListItem = MutableLiveData<NoticeListResponse>()
+
+    fun showNoticeList(studyId: Int) {
+        getNoticeList(studyId) { data ->
+            noticeListItem.value = data
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun getNoticeList(studyId: Int, callback: (data: NoticeListResponse) -> Unit) {
+
+        ApiModule.provideNoticeApi()
+            .getNoticeList(
+                bearerToken = "Bearer $token",
+                studyId = studyId
+            )
+            .networkSchedulers()
+            .subscribe({ response ->
+                if (response.data != null) {
+                    callback(response.data)
+                }
+            }, {
+                error(it)
+            })
+    }
+}
