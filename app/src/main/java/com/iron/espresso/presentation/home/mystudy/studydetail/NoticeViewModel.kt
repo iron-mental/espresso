5f00b1f6d6659464c8d14c1a2f2878ec66717548
ko0chan <kychan1320@gmail.com>
2020-12-08 22:31:24 +0900

    chore(NoticeViewModel) : AuthHolder 적용

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeViewModel.kt
@@ -2,15 +2,13 @@ package com.iron.espresso.presentation.home.mystudy.studydetail
 
 import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
+import com.iron.espresso.AuthHolder
 import com.iron.espresso.base.BaseViewModel
 import com.iron.espresso.di.ApiModule
 import com.iron.espresso.ext.networkSchedulers
 import com.iron.espresso.model.response.notice.NoticeListResponse
 
 class NoticeViewModel : BaseViewModel() {
-
-    private val token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksImVtYWlsIjoicmhkdWRja3NAbmF2ZXIuY29tIiwibmlja25hbWUiOiLqs6DsmIHssKwiLCJpYXQiOjE2MDU4NDk0MzMsImV4cCI6MTYwNzE0NTQzMywiaXNzIjoidGVybWluYWwtc2VydmVyIiwic3ViIjoidXNlckluZm8tYWNjZXNzIn0.Eptf9T9Z_c-VmIUqLNV5CKAN-ftm1sZSwOzs91SrIr0"
 
     val noticeListItem = MutableLiveData<NoticeListResponse>()
 
@@ -25,7 +23,7 @@ class NoticeViewModel : BaseViewModel() {
 
         ApiModule.provideNoticeApi()
             .getNoticeList(
-                bearerToken = "Bearer $token",
+                bearerToken = AuthHolder.bearerToken,
                 studyId = studyId
             )
             .networkSchedulers()
