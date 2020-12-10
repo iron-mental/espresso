64bb1e389d8a01674aa6215dddcf1cbc127af109
ko0chan <kychan1320@gmail.com>
2020-12-10 17:38:45 +0900

    feat(...) : Toolbar Item 추가

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeCreateActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/NoticeCreateActivity.kt
@@ -3,6 +3,10 @@ package com.iron.espresso.presentation.home.mystudy.studydetail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseActivity
 import com.iron.espresso.databinding.ActivityNoticeCreateBinding
@@ -12,6 +16,28 @@ class NoticeCreateActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setToolbarTitle("공지사항 작성 화면")
+        setNavigationIcon(R.drawable.ic_back_24)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_notice_create, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+            }
+            R.id.create_notice -> {
+                Toast.makeText(this, "공지사항 작성이 완료됐습니다", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+        return true
     }
 
     companion object {
