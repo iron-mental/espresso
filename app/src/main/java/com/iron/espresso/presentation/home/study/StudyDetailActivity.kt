fabdff3ffb02f74568135f79a4557657d62c203f
ko0chan <kychan1320@gmail.com>
2020-12-16 21:34:41 +0900

    chore(StudyDetailActivity) : 툴바 세팅 및 액션

--- app/src/main/java/com/iron/espresso/presentation/home/study/StudyDetailActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/StudyDetailActivity.kt
@@ -3,6 +3,7 @@ package com.iron.espresso.presentation.home.study
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseActivity
 import com.iron.espresso.databinding.ActivityStudyDetailBinding
@@ -12,6 +13,19 @@ class StudyDetailActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setToolbarTitle("스터디 상세화면")
+        setNavigationIcon(R.drawable.ic_back_24)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+            }
+        }
+        return true
     }
 
     companion object {
