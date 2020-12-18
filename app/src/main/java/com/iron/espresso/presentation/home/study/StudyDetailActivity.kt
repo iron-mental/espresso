c37481fba796d2d4cad573025b6e15770f7ccc40
ko0chan <kychan1320@gmail.com>
2020-12-18 19:27:24 +0900

    feat(...) : 스터디 구성원 이미지, 닉네임 뷰 동적 생성

--- app/src/main/java/com/iron/espresso/presentation/home/study/StudyDetailActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/StudyDetailActivity.kt
@@ -3,12 +3,20 @@ package com.iron.espresso.presentation.home.study
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.LazyHeaders
+import com.bumptech.glide.request.RequestOptions
+import com.iron.espresso.AuthHolder
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseActivity
 import com.iron.espresso.databinding.ActivityStudyDetailBinding
+import kotlinx.android.synthetic.main.item_member.view.member_image
+import kotlinx.android.synthetic.main.item_member.view.member_nickname
 
 class StudyDetailActivity :
     BaseActivity<ActivityStudyDetailBinding>(R.layout.activity_study_detail) {
@@ -21,15 +29,36 @@ class StudyDetailActivity :
         setToolbarTitle("스터디 상세화면")
         setNavigationIcon(R.drawable.ic_back_24)
 
-        viewModel.getStudy(193)
+        viewModel.getStudy(2)
 
         viewModel.studyDetail.observe(this, Observer { studyDetail ->
             binding.introduceDetail.text = studyDetail.introduce
             binding.proceedDetail.text = studyDetail.progress
             binding.timeDetail.text = studyDetail.studyTime
             binding.placeDetail.text = studyDetail.locationResponse?.addressName
-        })
 
+            studyDetail.participateResponse?.forEach { memberList ->
+                val memberView = LayoutInflater.from(this)
+                    .inflate(R.layout.item_member, binding.memberContainer, false)
+
+                memberView.member_nickname.text = memberList.nickname
+
+                Glide.with(this)
+                    .load(
+                        GlideUrl(
+                            memberList.image,
+                            LazyHeaders.Builder()
+                                .addHeader("Authorization", AuthHolder.bearerToken)
+                                .build()
+                        )
+                    )
+                    .apply(RequestOptions.circleCropTransform())
+                    .error(R.drawable.dummy_image)
+                    .into(memberView.member_image)
+
+                binding.memberContainer.addView(memberView)
+            }
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
