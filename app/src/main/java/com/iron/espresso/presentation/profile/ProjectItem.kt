228cd81498e4e1d88aa2f9044b7cc01ca7bfb81f
josh <josh@watcha.com>
2020-12-21 02:52:48 +0900

    feat(...) : 프로젝트 레이아웃 추가되도록

--- app/src/main/java/com/iron/espresso/presentation/profile/ProjectItem.kt
+++ app/src/main/java/com/iron/espresso/presentation/profile/ProjectItem.kt
@@ -1,10 +1,10 @@
 package com.iron.espresso.presentation.profile
 
 data class ProjectItem(
-    val id: Int,
-    val title: String,
-    val contents: String,
-    val githubUrl: String,
-    val appStoreUrl: String,
-    val playStoreUrl: String
+    val id: Int = -1,
+    val title: String = "",
+    val contents: String = "",
+    val githubUrl: String = "",
+    val appStoreUrl: String = "",
+    val playStoreUrl: String = ""
 )
--- app/src/main/java/com/iron/espresso/presentation/profile/edit/EditProjectViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/profile/edit/EditProjectViewModel.kt
@@ -11,4 +11,29 @@ class EditProjectViewModel @ViewModelInject constructor() : BaseViewModel() {
     private val _projectItemList = MutableLiveData<List<ProjectItem>>()
     val projectItemList: LiveData<List<ProjectItem>> get() = _projectItemList
 
+    private val _firstItem = MutableLiveData<ProjectItem>()
+    val firstItem: LiveData<ProjectItem> get() = _firstItem
+
+    private val _secondItem = MutableLiveData<ProjectItem>()
+    val secondItem: LiveData<ProjectItem> get() = _secondItem
+
+    private val _thirdItem = MutableLiveData<ProjectItem>()
+    val thirdItem: LiveData<ProjectItem> get() = _thirdItem
+
+    fun addProject() {
+        val item = ProjectItem()
+        _projectItemList.value = projectItemList.value.orEmpty() + item
+        when (projectItemList.value?.size) {
+            1 -> {
+                _firstItem.value = item
+            }
+            2 -> {
+                _secondItem.value = item
+            }
+            3 -> {
+                _thirdItem.value = item
+            }
+        }
+
+    }
 }
