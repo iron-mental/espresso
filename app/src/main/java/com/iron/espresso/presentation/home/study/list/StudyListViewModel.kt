65ad29a837b6511922465b999567c3133e759fae
ko0chan <kychan1320@gmail.com>
2020-12-31 20:48:49 +0900

    chore(StudyListViewModel) : disposable

--- app/src/main/java/com/iron/espresso/presentation/home/study/list/StudyListViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/list/StudyListViewModel.kt
@@ -8,20 +8,20 @@ import com.iron.espresso.AuthHolder
 import com.iron.espresso.Logger
 import com.iron.espresso.base.BaseViewModel
 import com.iron.espresso.ext.networkSchedulers
+import com.iron.espresso.ext.plusAssign
 import com.iron.espresso.ext.toErrorResponse
 import com.iron.espresso.model.api.StudyApi
 import com.iron.espresso.model.response.study.StudyListResponse
 import retrofit2.HttpException
 
-class StudyListViewModel @ViewModelInject constructor(val studyApi: StudyApi) : BaseViewModel() {
+class StudyListViewModel @ViewModelInject constructor(private val studyApi: StudyApi) : BaseViewModel() {
 
     private val _studyList = MutableLiveData<StudyListResponse>()
     val studyList: LiveData<StudyListResponse>
         get() = _studyList
 
-    @SuppressLint("CheckResult")
     fun getStudyList(category: String, sort: String) {
-        studyApi
+        compositeDisposable += studyApi
             .getStudyList(
                 bearerToken = AuthHolder.bearerToken,
                 category = category,
