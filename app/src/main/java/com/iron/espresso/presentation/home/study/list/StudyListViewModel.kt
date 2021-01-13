44ed95e231b48a5da31f64a11503c1816a677ef5
ko0chan <kychan1320@gmail.com>
2021-01-13 16:09:08 +0900

    feat(StudyListViewModel) : 스크롤시 아이템을 추가시키는 페이지네이션 추가

--- app/src/main/java/com/iron/espresso/presentation/home/study/list/StudyListViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/list/StudyListViewModel.kt
@@ -20,6 +20,65 @@ class StudyListViewModel @ViewModelInject constructor(private val studyApi: Stud
     val studyList: LiveData<List<StudyItem>>
         get() = _studyList
 
+    private val _scrollItem = MutableLiveData<List<StudyItem>>()
+    val scrollItem: LiveData<List<StudyItem>>
+        get() = _scrollItem
+
+    private val allList = mutableListOf<StudyItem>()
+    private var moreItemSize = -1
+    private var totalSize = -1
+    private var itemCount = -1
+    private var loading = false
+
+
+    private fun firstItemResult(studyList: List<StudyItem>): List<StudyItem> {
+        val list: MutableList<StudyItem> = mutableListOf()
+        moreItemSize = VISIBLE_ITEM_SIZE
+        itemCount = 0
+        loading = false
+        allList.clear()
+        allList.addAll(studyList)
+        totalSize = studyList.size
+
+        return if (totalSize <= VISIBLE_ITEM_SIZE) {
+            studyList
+        } else {
+            loading = true
+            for (i in 0 until VISIBLE_ITEM_SIZE) {
+                itemCount++
+                list.add(allList[i])
+            }
+            list
+        }
+    }
+
+    private fun scrollMoreItem(): String {
+        val list = mutableListOf<Int>()
+        moreItemSize += VISIBLE_ITEM_SIZE
+        return when {
+            moreItemSize <= totalSize -> {
+                for (i in itemCount until itemCount + VISIBLE_ITEM_SIZE) {
+                    itemCount++
+                    list.add(allList[i].id)
+                }
+                list.joinToString(",")
+            }
+            moreItemSize > totalSize -> {
+                loading = false
+                for (i in itemCount until allList.size) {
+                    itemCount++
+                    list.add(allList[i].id)
+                }
+
+                list.joinToString(",")
+            }
+            else -> {
+                loading = false
+                error("validation error")
+            }
+        }
+    }
+
     fun getStudyList(category: String, sort: String) {
         compositeDisposable += studyApi
             .getStudyList(
@@ -30,8 +89,14 @@ class StudyListViewModel @ViewModelInject constructor(private val studyApi: Stud
             .networkSchedulers()
             .subscribe({
                 Logger.d("$it")
-                _studyList.value = it.data?.map { studyResponse ->
-                    studyResponse.toStudyItem()
+                if (it.data != null) {
+                    val studyList =
+                        firstItemResult(
+                            it.data.map { studyResponse ->
+                                studyResponse.toStudyItem()
+                            }
+                        )
+                    _studyList.value = studyList
                 }
             }, {
                 Logger.d("$it")
@@ -42,4 +107,30 @@ class StudyListViewModel @ViewModelInject constructor(private val studyApi: Stud
             })
     }
 
+    fun getStudyListPaging() {
+        if (loading) {
+            compositeDisposable += studyApi
+                .getStudy(
+                    bearerToken = AuthHolder.bearerToken,
+                    studyIds = scrollMoreItem()
+                )
+                .networkSchedulers()
+                .subscribe({
+                    _scrollItem.value = it.data?.map { studyResponse ->
+                        studyResponse.toStudyItem()
+                    }
+                    Logger.d("$it")
+                }, {
+                    Logger.d("$it")
+                    val errorResponse = (it as? HttpException)?.toErrorResponse()
+                    if (errorResponse != null) {
+                        Logger.d("${errorResponse.message}")
+                    }
+                })
+        }
+    }
+
+    companion object {
+        private const val VISIBLE_ITEM_SIZE = 10
+    }
 }
