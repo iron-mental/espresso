4d06dbe825d2c8a3ed44c518804d0417907cadf5
ko0chan <kychan1320@gmail.com>
2021-01-13 16:07:56 +0900

    feat(StudyListViewHolder) : GlideUrl이용하여 이미지 로드

--- app/src/main/java/com/iron/espresso/presentation/home/study/adapter/StudyListViewHolder.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/adapter/StudyListViewHolder.kt
@@ -5,8 +5,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.LazyHeaders
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.iron.espresso.AuthHolder
 import com.iron.espresso.R
 import com.iron.espresso.data.model.StudyItem
 import com.iron.espresso.databinding.ItemStudyListBinding
@@ -30,13 +33,35 @@ class StudyListViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
             date.text = item.createdAt
 
             Glide.with(itemView)
-                .load(item.image)
+                .load(
+                    if (item.image.isNullOrEmpty()) {
+                        R.drawable.dummy_image
+                    } else {
+                        GlideUrl(
+                            item.image,
+                            LazyHeaders.Builder()
+                                .addHeader("Authorization", AuthHolder.bearerToken)
+                                .build()
+                        )
+                    }
+                )
                 .transform(CenterCrop(), RoundedCorners(30))
                 .error(R.drawable.dummy_image)
                 .into(image)
 
             Glide.with(itemView)
-                .load(item.leaderImage)
+                .load(
+                    if (item.leaderImage.isNullOrEmpty()) {
+                        R.drawable.dummy_image
+                    } else {
+                        GlideUrl(
+                            item.leaderImage,
+                            LazyHeaders.Builder()
+                                .addHeader("Authorization", AuthHolder.bearerToken)
+                                .build()
+                        )
+                    }
+                )
                 .circleCrop()
                 .error(R.drawable.dummy_image)
                 .into(profileImage)
