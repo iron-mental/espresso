3b6386aeed2901a385a5818268c3619bcf9e2357
josh <josh@watcha.com>
2021-01-17 09:58:23 +0900

    feat(...) : 프로필 경력 수정

--- app/src/main/java/com/iron/espresso/presentation/profile/edit/EditCareerViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/profile/edit/EditCareerViewModel.kt
@@ -1,7 +1,70 @@
 package com.iron.espresso.presentation.profile.edit
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.iron.espresso.AuthHolder
+import com.iron.espresso.Logger
 import com.iron.espresso.base.BaseViewModel
+import com.iron.espresso.ext.Event
+import com.iron.espresso.ext.networkSchedulers
+import com.iron.espresso.ext.plusAssign
+import com.iron.espresso.ext.toErrorResponse
+import com.iron.espresso.model.source.remote.ModifyUserCareerRequest
+import com.iron.espresso.model.source.remote.UserRemoteDataSource
 
-class EditCareerViewModel @ViewModelInject constructor() : BaseViewModel() {
+class EditCareerViewModel @ViewModelInject constructor(private val remoteDataSource: UserRemoteDataSource) :
+    BaseViewModel() {
+
+    private var initTitle = ""
+    private var initContents = ""
+    val title = MutableLiveData<String>()
+    val contents = MutableLiveData<String>()
+
+    private val _successEvent = MutableLiveData<Event<Boolean>>()
+    val successEvent: LiveData<Event<Boolean>> get() = _successEvent
+
+    fun initProfileData(title: String, contents: String) {
+        initTitle = title
+        initContents = contents
+        this.title.value = title
+        this.contents.value = contents
+    }
+
+    fun modifyInfo() {
+        if (title.value == initTitle && contents.value == initContents) {
+            _successEvent.value = Event(false)
+            return
+        }
+
+        val title = title.value.orEmpty()
+        val contents = contents.value.orEmpty()
+        val bearerToken = AuthHolder.bearerToken
+        val id = AuthHolder.id ?: return
+
+        if (title.isNotEmpty() && contents.isNotEmpty()) {
+            compositeDisposable += remoteDataSource.modifyUserCareer(
+                bearerToken,
+                id,
+                ModifyUserCareerRequest(title, contents)
+            )
+                .networkSchedulers()
+                .subscribe({
+                    if (it.result) {
+                        _successEvent.value = Event(true)
+                    } else if (!it.message.isNullOrEmpty()) {
+                        _toastMessage.value = Event(it.message)
+                    }
+                }, {
+                    Logger.d("$it")
+                    it.toErrorResponse()?.let { errorResponse ->
+                        if (!errorResponse.message.isNullOrEmpty()) {
+                            _toastMessage.value = Event(errorResponse.message)
+                        }
+                    }
+                })
+        }
+    }
+
+
 }
