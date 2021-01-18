a64a443bc8486722318f70abb7f2037b9fcc1edf
ko0chan <kychan1320@gmail.com>
2021-01-18 17:08:05 +0900

    feat(StudyInfoViewModel) : 스터디 정보 뷰 모델 구성

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/StudyInfoFragment.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/StudyInfoFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseFragment
 import com.iron.espresso.databinding.FragmentStudyInfoBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StudyInfoFragment : BaseFragment<FragmentStudyInfoBinding>(R.layout.fragment_study_info) {
+
+    private val viewModel by viewModels<StudyInfoViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
