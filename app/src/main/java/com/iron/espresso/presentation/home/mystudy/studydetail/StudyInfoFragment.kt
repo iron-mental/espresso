2e45f9a08fbb3cd8d7b8ededc334ef1b545ce8bc
ko0chan <kychan1320@gmail.com>
2021-01-18 16:39:11 +0900

    feat(StudyInfoFragment) : 옵션 메뉴 선택 대응

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/StudyInfoFragment.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/StudyInfoFragment.kt
@@ -1,7 +1,11 @@
 package com.iron.espresso.presentation.home.mystudy.studydetail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseFragment
 import com.iron.espresso.databinding.FragmentStudyInfoBinding
@@ -10,6 +14,19 @@ class StudyInfoFragment : BaseFragment<FragmentStudyInfoBinding>(R.layout.fragme
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                activity?.finish()
+            }
+
+            else -> {
+                Toast.makeText(context, "${item.title}", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
     }
 
     companion object {
