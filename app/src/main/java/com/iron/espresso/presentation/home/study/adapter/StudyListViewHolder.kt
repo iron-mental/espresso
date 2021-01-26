67c99ab95f1eafe0d40dfcb152d0aa5057741e31
ko0chan <kychan1320@gmail.com>
2021-01-26 14:56:52 +0900

    feat(...) : 헤더 지정 ImageExt 추가 및 적용

--- app/src/main/java/com/iron/espresso/presentation/home/study/adapter/StudyListViewHolder.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/adapter/StudyListViewHolder.kt
@@ -4,16 +4,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.load.model.LazyHeaders
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.iron.espresso.AuthHolder
 import com.iron.espresso.R
-import com.iron.espresso.data.model.CreateStudyItem
 import com.iron.espresso.data.model.StudyItem
 import com.iron.espresso.databinding.ItemStudyListBinding
+import com.iron.espresso.ext.setCircleImage
+import com.iron.espresso.ext.setRadiusImage
 
 class StudyListViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(
@@ -32,39 +27,17 @@ class StudyListViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
             location.text = item.sigungu
             date.text = item.createdAt
 
-            Glide.with(itemView)
-                .load(
-                    if (item.image.isNullOrEmpty()) {
-                        R.drawable.dummy_image
-                    } else {
-                        GlideUrl(
-                            item.image,
-                            LazyHeaders.Builder()
-                                .addHeader("Authorization", AuthHolder.bearerToken)
-                                .build()
-                        )
-                    }
-                )
-                .transform(CenterCrop(), RoundedCorners(30))
-                .error(R.drawable.dummy_image)
-                .into(image)
+            if (item.image.isNullOrEmpty()) {
+                image.setImageResource(R.drawable.dummy_image)
+            } else {
+                image.setRadiusImage(item.image)
+            }
 
-            Glide.with(itemView)
-                .load(
-                    if (item.leaderImage.isNullOrEmpty()) {
-                        R.drawable.dummy_image
-                    } else {
-                        GlideUrl(
-                            item.leaderImage,
-                            LazyHeaders.Builder()
-                                .addHeader("Authorization", AuthHolder.bearerToken)
-                                .build()
-                        )
-                    }
-                )
-                .circleCrop()
-                .error(R.drawable.dummy_image)
-                .into(profileImage)
+            if (item.leaderImage.isNullOrEmpty()) {
+                profileImage.setImageResource(R.drawable.dummy_image)
+            } else {
+                profileImage.setCircleImage(item.leaderImage)
+            }
         }
     }
 }
