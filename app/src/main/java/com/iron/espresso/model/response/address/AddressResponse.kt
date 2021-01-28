50f050739c67af5566f9d42583ba501fa40769d5
josh <josh@watcha.com>
2021-01-29 00:53:22 +0900

    fix(AddressResponse.kt) : 서버 컬럼명이 바꼈나보다

--- app/src/main/java/com/iron/espresso/model/response/address/AddressResponse.kt
+++ app/src/main/java/com/iron/espresso/model/response/address/AddressResponse.kt
@@ -4,8 +4,8 @@ package com.iron.espresso.model.response.address
 import com.google.gson.annotations.SerializedName
 
 data class AddressResponse(
-    @SerializedName("si")
+    @SerializedName("sido")
     val si: String?,
-    @SerializedName("gunGu")
+    @SerializedName("sigungu")
     val gunGu: List<String>?
 )
