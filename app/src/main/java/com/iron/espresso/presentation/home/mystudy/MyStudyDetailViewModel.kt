b033f96e5043718482ade9d8af13e15ce3d7b6f5
ko0chan <kychan1320@gmail.com>
2021-02-08 16:42:55 +0900

    feat(MyStudyDetailViewModel) : 스터디 상세 api 추가 및 적용

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/MyStudyDetailViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/MyStudyDetailViewModel.kt
@@ -1,8 +1,13 @@
 package com.iron.espresso.presentation.home.mystudy
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.iron.espresso.AuthHolder
 import com.iron.espresso.Logger
 import com.iron.espresso.base.BaseViewModel
+import com.iron.espresso.data.model.StudyDetailItem
+import com.iron.espresso.di.ApiModule
 import com.iron.espresso.domain.repo.StudyRepository
 import com.iron.espresso.ext.Event
 import com.iron.espresso.ext.networkSchedulers
@@ -12,6 +17,30 @@ import retrofit2.HttpException
 
 class MyStudyDetailViewModel @ViewModelInject constructor(private val studyRepository: StudyRepository) :
     BaseViewModel() {
+
+    private val _studyDetail = MutableLiveData<StudyDetailItem>()
+    val studyDetail: LiveData<StudyDetailItem>
+        get() = _studyDetail
+
+    fun getStudy(studyId: Int) {
+        compositeDisposable += ApiModule.provideStudyApi()
+            .getStudyDetail(
+                bearerToken = AuthHolder.bearerToken,
+                studyId = studyId
+            )
+            .networkSchedulers()
+            .subscribe({
+                if (it.data != null) {
+                    _studyDetail.value = it.data.toStudyDetailItem()
+                }
+                Logger.d("$it")
+            }, {
+                val errorResponse = (it as? HttpException)?.toErrorResponse()
+                if (errorResponse != null) {
+                    Logger.d("$errorResponse")
+                }
+            })
+    }
 
     fun leaveStudy(studyId: Int) {
         compositeDisposable += studyRepository
