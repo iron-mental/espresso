0f02d58db525884f09c17e265e45b3f542a8c30c
ko0chan <kychan1320@gmail.com>
2021-02-11 00:58:13 +0900

    feat(...) : 스터디 장 위임 Activity 추가

---
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/DelegateLeaderActivity.kt
@@ -0,0 +1,21 @@
+package com.iron.espresso.presentation.home.mystudy.studydetail
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.iron.espresso.R
+import com.iron.espresso.base.BaseActivity
+import com.iron.espresso.databinding.ActivityDelegateLeaderBinding
+
+class DelegateLeaderActivity :
+    BaseActivity<ActivityDelegateLeaderBinding>(R.layout.activity_delegate_leader) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    companion object {
+        fun getIntent(context: Context) =
+            Intent(context, DelegateLeaderActivity::class.java)
+    }
+}
