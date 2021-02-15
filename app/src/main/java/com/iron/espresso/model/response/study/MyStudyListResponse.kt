1e8dea49c98e02cce630bbe3a5fd9329342d7f0b
ko0chan <kychan1320@gmail.com>
2021-02-15 15:47:49 +0900

    fix(...) : 내 스터디 목록 api변경 대응

--- app/src/main/java/com/iron/espresso/model/response/study/MyStudyListResponse.kt
+++ app/src/main/java/com/iron/espresso/model/response/study/MyStudyListResponse.kt
@@ -1,4 +1,20 @@
 package com.iron.espresso.model.response.study
 
+import com.google.gson.annotations.SerializedName
+import com.iron.espresso.data.model.BadgeItem
+import com.iron.espresso.data.model.MyStudyListItem
 
-class MyStudyListResponse : ArrayList<MyStudyResponse>()
+data class MyStudyListResponse(
+    @SerializedName("badge")
+    val badgeResponse: BadgeResponse?,
+    @SerializedName("studyList")
+    val myStudyResponse: ArrayList<MyStudyResponse>?
+) {
+    fun toMyStudyListItem() =
+        MyStudyListItem(
+            badge = badgeResponse?.toBadgeItem() ?: BadgeItem(),
+            myStudyItem = myStudyResponse?.map {
+                it.toMyStudyItem()
+            }.orEmpty()
+        )
+}
