754451bd66249c175626da9e41c8095164e951c3
ko0chan <kychan1320@gmail.com>
2021-02-15 20:25:34 +0900

    feat(...) : ParticipateAdapter, 리사이클러뷰 어댑터 연결

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/DelegateLeaderActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/DelegateLeaderActivity.kt
@@ -8,12 +8,18 @@ import com.iron.espresso.base.BaseActivity
 import com.iron.espresso.data.model.ParticipateItem
 import com.iron.espresso.databinding.ActivityDelegateLeaderBinding
 import com.iron.espresso.presentation.home.mystudy.MyStudyDetailActivity
+import com.iron.espresso.presentation.home.mystudy.adapter.ParticipateAdapter
 
 class DelegateLeaderActivity :
     BaseActivity<ActivityDelegateLeaderBinding>(R.layout.activity_delegate_leader) {
 
+    private val participateAdapter = ParticipateAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.participateList.adapter = participateAdapter
+        participateAdapter.setItemList(intent.getSerializableExtra(PARTICIPATE_LIST) as List<ParticipateItem>)
     }
 
     companion object {
