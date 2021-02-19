0ba6cf61420abcbd92721456f0630db40430b554
ko0chan <kychan1320@gmail.com>
2021-02-19 19:23:53 +0900

    feat(...) : 장소 미 수정시 request 안함

--- app/src/main/java/com/iron/espresso/data/model/LocalItem.kt
+++ app/src/main/java/com/iron/espresso/data/model/LocalItem.kt
@@ -3,8 +3,8 @@ package com.iron.espresso.data.model
 import java.io.Serializable
 
 data class LocalItem(
-    val lat: Double = -1.0,
-    val lng: Double = -1.0,
+    val lat: Double? = null,
+    val lng: Double? = null,
     val sido: String = "",
     val sigungu: String = "",
     val addressName: String = "",
