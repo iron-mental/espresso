7fa97a5f23d62bf5e26c3efe46514d38d98c6afd
ko0chan <kychan1320@gmail.com>
2021-03-05 19:45:00 +0900

    feat(...) : 스터디 삭제, 나가기, 위임 dialog 추가

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/DelegateLeaderActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/DelegateLeaderActivity.kt
@@ -1,10 +1,13 @@
 package com.iron.espresso.presentation.home.mystudy.studydetail
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.fragment.app.commit
+import androidx.fragment.app.setFragmentResultListener
 import com.iron.espresso.DEF_VALUE
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseActivity
@@ -12,6 +15,7 @@ import com.iron.espresso.data.model.ParticipateItem
 import com.iron.espresso.databinding.ActivityDelegateLeaderBinding
 import com.iron.espresso.ext.EventObserver
 import com.iron.espresso.ext.toast
+import com.iron.espresso.presentation.home.apply.ConfirmDialog
 import com.iron.espresso.presentation.home.mystudy.MyStudyDetailActivity
 import com.iron.espresso.presentation.home.mystudy.adapter.ParticipateAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,6 +28,7 @@ class DelegateLeaderActivity :
     private val participateAdapter = ParticipateAdapter()
     private var studyId = -1
     private var memberList = listOf<ParticipateItem>()
+    private var authority: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,16 +39,14 @@ class DelegateLeaderActivity :
         studyId =
             intent.getIntExtra(MyStudyDetailActivity.STUDY_ID, DEF_VALUE)
         memberList = intent.getSerializableExtra(PARTICIPATE_LIST) as List<ParticipateItem>
+        authority = intent.getStringExtra(KEY_AUTHORITY)
 
         binding.participateList.adapter = participateAdapter
 
         participateAdapter.apply {
             setItemList(memberList)
             setItemClickListener { participateItem ->
-                viewModel.delegateStudyLeader(
-                    studyId,
-                    participateItem.userId
-                )
+                showDelegateLeaderDialog(participateItem.userId)
             }
         }
 
@@ -60,6 +63,32 @@ class DelegateLeaderActivity :
         }
     }
 
+    private fun showDelegateLeaderDialog(userId: Int) {
+        val dialog = ConfirmDialog.newInstance(getString(R.string.dialog_delegate_leader_title))
+
+        dialog.show(supportFragmentManager, dialog::class.java.simpleName)
+
+        dialog.setFragmentResultListener(dialog::class.java.simpleName) { _: String, bundle: Bundle ->
+            val result = bundle.get(ConfirmDialog.RESULT)
+
+            if (result == Activity.RESULT_OK) {
+                viewModel.delegateStudyLeader(studyId, userId)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        val fragment = supportFragmentManager.fragments.find { it is ConfirmDialog }
+
+        if (fragment != null) {
+            supportFragmentManager.commit {
+                remove(fragment)
+            }
+        } else {
+            return super.onBackPressed()
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
@@ -72,9 +101,11 @@ class DelegateLeaderActivity :
     companion object {
         private const val TOOLBAR_TITLE = "스터디 장 위임화면"
         private const val PARTICIPATE_LIST = "participateList"
-        fun getIntent(context: Context, studyId: Int, participateList: ArrayList<ParticipateItem>) =
+        private const val KEY_AUTHORITY = "AUTHORITY"
+        fun getIntent(context: Context, studyId: Int, participateList: ArrayList<ParticipateItem>, authority: String) =
             Intent(context, DelegateLeaderActivity::class.java)
                 .putExtra(MyStudyDetailActivity.STUDY_ID, studyId)
                 .putExtra(PARTICIPATE_LIST, participateList)
+                .putExtra(KEY_AUTHORITY, authority)
     }
 }
