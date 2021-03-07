88dda50d1479b533feb778f18b0236521523555c
josh <josh@watcha.com>
2021-03-07 23:19:52 +0900

    feat(AlertListActivity.kt) : 뒤로가기 아이콘 적용

--- app/src/main/java/com/iron/espresso/presentation/home/alert/AlertListActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/alert/AlertListActivity.kt
@@ -3,6 +3,7 @@ package com.iron.espresso.presentation.home.alert
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.viewModels
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseActivity
@@ -24,6 +25,7 @@ class AlertListActivity : BaseActivity<ActivityAlertListBinding>(R.layout.activi
         super.onCreate(savedInstanceState)
 
         setToolbarTitle(R.string.alert)
+        setNavigationIcon(R.drawable.ic_back_24)
 
         setupView()
         setupViewModel()
@@ -34,6 +36,16 @@ class AlertListActivity : BaseActivity<ActivityAlertListBinding>(R.layout.activi
         with(binding) {
             alertList.adapter = adapter
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupViewModel() {
