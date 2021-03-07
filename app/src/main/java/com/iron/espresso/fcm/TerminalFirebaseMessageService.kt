90e916e880ce9962773aa367cf471061bb1fc4d0
josh <josh@watcha.com>
2021-03-08 01:35:36 +0900

    feat(TerminalFirebaseMessageService.kt) : 푸시토큰 재발급 처리

--- app/src/main/java/com/iron/espresso/fcm/TerminalFirebaseMessageService.kt
+++ app/src/main/java/com/iron/espresso/fcm/TerminalFirebaseMessageService.kt
@@ -3,12 +3,25 @@ package com.iron.espresso.fcm
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.iron.espresso.Logger
+import com.iron.espresso.di.ApiModule
+import io.reactivex.disposables.Disposable
 
 class TerminalFirebaseMessageService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
 
+        var disposable: Disposable? = null
+
+        disposable = ApiModule.provideUserApi()
+            .refreshPushToken()
+            .subscribe({
+                Logger.d("Refresh Success")
+                disposable?.dispose()
+            }, {
+                Logger.d("$it")
+                disposable?.dispose()
+            })
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
