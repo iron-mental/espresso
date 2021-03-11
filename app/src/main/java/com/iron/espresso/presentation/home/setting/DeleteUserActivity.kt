1bfcacef9d2fa8b08c60e8b82c4fdbce3f3032c6
ko0chan <kychan1320@gmail.com>
2021-03-12 01:17:33 +0900

    feat(...) : 회원탈퇴 dialog 추가

--- app/src/main/java/com/iron/espresso/presentation/home/setting/DeleteUserActivity.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/setting/DeleteUserActivity.kt
@@ -5,12 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
+import androidx.fragment.app.setFragmentResultListener
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseActivity
 import com.iron.espresso.databinding.ActivityDeleteUserBinding
 import com.iron.espresso.ext.EventObserver
 import com.iron.espresso.ext.onEditorEnterAction
 import com.iron.espresso.ext.toast
+import com.iron.espresso.presentation.home.apply.ConfirmDialog
 import com.iron.espresso.presentation.sign.IntroActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -36,10 +38,10 @@ class DeleteUserActivity : BaseActivity<ActivityDeleteUserBinding>(R.layout.acti
             imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
 
             passwordInputView.onEditorEnterAction {
-                deleteUserViewModel.membershipWithdrawal(emailInputView.text.toString(), passwordInputView.text.toString())
+                showDeleteUserDialog(emailInputView.text.toString(), passwordInputView.text.toString())
             }
             confirmButton.setOnClickListener {
-                deleteUserViewModel.membershipWithdrawal(emailInputView.text.toString(), passwordInputView.text.toString())
+                showDeleteUserDialog(emailInputView.text.toString(), passwordInputView.text.toString())
             }
         }
     }
@@ -55,6 +57,20 @@ class DeleteUserActivity : BaseActivity<ActivityDeleteUserBinding>(R.layout.acti
                         .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 )
             })
+        }
+    }
+
+    private fun showDeleteUserDialog(email: String, password: String) {
+        val dialog = ConfirmDialog.newInstance(getString(R.string.dialog_delete_user_title))
+
+        dialog.show(supportFragmentManager, dialog::class.java.simpleName)
+
+        dialog.setFragmentResultListener(dialog::class.java.simpleName) { _: String, bundle: Bundle ->
+            val result = bundle.get(ConfirmDialog.RESULT)
+
+            if (result == RESULT_OK) {
+                deleteUserViewModel.membershipWithdrawal(email, password)
+            }
         }
     }
 
