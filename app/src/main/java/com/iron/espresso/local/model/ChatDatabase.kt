11e839e90109d7b51d4dba0daf03f9e7be5ca722
ko0chan <kychan1320@gmail.com>
2021-03-21 17:45:17 +0900

    chore(ChatDatabase.kt) : 미사용 코드 제거

--- app/src/main/java/com/iron/espresso/local/model/ChatDatabase.kt
+++ app/src/main/java/com/iron/espresso/local/model/ChatDatabase.kt
@@ -1,34 +1,10 @@
 package com.iron.espresso.local.model
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [ChatEntity::class], version = 1)
 abstract class ChatDatabase : RoomDatabase() {
 
     abstract fun chatDao(): ChatDao
-
-    companion object {
-
-        @Volatile
-        private var INSTANCE: ChatDatabase? = null
-
-        fun getDatabase(context: Context): ChatDatabase {
-
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    ChatDatabase::class.java,
-                    "chat_database"
-                )
-                    .allowMainThreadQueries()
-                    .build()
-                INSTANCE = instance
-
-                instance
-            }
-        }
-    }
 }
