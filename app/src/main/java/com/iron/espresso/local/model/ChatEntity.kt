ea130bfa85051ea5fc98bccba2442680c38a7e05
ko0chan <kychan1320@gmail.com>
2021-03-22 23:14:41 +0900

    feat(ChatEntity) : ChatEntity 데이터 변환 함수 추가

--- app/src/main/java/com/iron/espresso/local/model/ChatEntity.kt
+++ app/src/main/java/com/iron/espresso/local/model/ChatEntity.kt
@@ -3,6 +3,8 @@ package com.iron.espresso.local.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.iron.espresso.domain.entity.Chat
+import com.iron.espresso.domain.entity.ChatUser
 
 @Entity(tableName = "chat_table")
 data class ChatEntity(
@@ -12,6 +14,43 @@ data class ChatEntity(
     @ColumnInfo(name = "user_id") val userId: Int,
     @ColumnInfo(name = "nickname") val nickname: String,
     @ColumnInfo(name = "message") val message: String,
-    @ColumnInfo(name = "time_stamp") val timeStamp: Long,
-    @ColumnInfo(name = "isMyChat") val isMyChat: Boolean
-)
+    @ColumnInfo(name = "time_stamp") val timeStamp: Long
+) {
+    companion object {
+        fun of(chatList: List<Chat>, userList: List<ChatUser>): List<ChatEntity> {
+
+            val list = mutableListOf<ChatEntity>()
+
+            chatList.forEach { chat ->
+                if (chat.userId == 0) {
+                    list.add(
+                        ChatEntity(
+                            uuid = chat.uuid,
+                            studyId = chat.studyId,
+                            userId = chat.userId,
+                            nickname = "",
+                            message = chat.message,
+                            timeStamp = chat.date
+                        )
+                    )
+                } else {
+                    userList.forEach { chatUser ->
+                        if (chat.userId == chatUser.userId) {
+                            list.add(
+                                ChatEntity(
+                                    uuid = chat.uuid,
+                                    studyId = chat.studyId,
+                                    userId = chat.userId,
+                                    nickname = chatUser.nickname,
+                                    message = chat.message,
+                                    timeStamp = chat.date
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+            return list
+        }
+    }
+}
