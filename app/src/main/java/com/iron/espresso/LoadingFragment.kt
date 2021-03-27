902b02b98861a03fb268e8e8897b813b88df475d
josh <josh@watcha.com>
2021-03-28 03:34:39 +0900

    fix(LoadingFragment.kt) : loading 보여주는 것 최소 100ms 이후에 나오도록

--- app/src/main/java/com/iron/espresso/LoadingFragment.kt
+++ app/src/main/java/com/iron/espresso/LoadingFragment.kt
@@ -1,10 +1,28 @@
 package com.iron.espresso
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import com.iron.espresso.base.BaseFragment
 import com.iron.espresso.databinding.FragmentLoadingBinding
+import com.iron.espresso.ext.plusAssign
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class LoadingFragment : BaseFragment<FragmentLoadingBinding>(R.layout.fragment_loading) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        compositeDisposable += Single.just(0)
+            .delay(100, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                binding.loadingView.isVisible = true
+            }, {
+
+            })
+    }
 
     companion object {
         fun newInstance() =
