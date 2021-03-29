6165537acae23b4af86a67cc6c5319cf9f5ae9d8
ko0chan <kychan1320@gmail.com>
2021-03-29 16:53:09 +0900

    feat(...) : System message View 분기 처리

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/chat/ChatAdapter.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/studydetail/chat/ChatAdapter.kt
@@ -3,27 +3,46 @@ package com.iron.espresso.presentation.home.mystudy.studydetail.chat
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 
-class ChatAdapter : ListAdapter<ChatItem, ChatViewHolder>(DIFF_CALLBACK) {
+class ChatAdapter : ListAdapter<ChatItem, RecyclerView.ViewHolder>(DIFF_CALLBACK) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
-        return ChatViewHolder(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType){
+            0 -> {
+                ChatSystemViewHolder(parent)
+            }
+            else -> {
+                ChatViewHolder(parent)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-        holder.bind(currentList[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(getItemViewType(position)) {
+            0 -> {
+                (holder as ChatSystemViewHolder).bind(currentList[position])
+            }
+            else -> {
+                (holder as ChatViewHolder).bind(currentList[position])
+            }
+        }
     }
 
     override fun onBindViewHolder(
-        holder: ChatViewHolder,
+        holder: RecyclerView.ViewHolder,
         position: Int,
         payloads: MutableList<Any>
     ) {
         if (payloads.contains(CHECK_ACTIVATE)) {
-            holder.checkActivation(currentList[position].sent)
+            (holder as ChatViewHolder).checkActivation(currentList[position].sent)
         } else {
             super.onBindViewHolder(holder, position, payloads)
         }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return currentList[position].userId
     }
 
     companion object {
