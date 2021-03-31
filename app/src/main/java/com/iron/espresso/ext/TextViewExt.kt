e57a2d9ec4ece9511bceb708143e4234583afe39
ko0chan <kychan1320@gmail.com>
2021-04-01 01:52:31 +0900

    chore(TextViewExt.kt) : 미사용 파일 삭제

--- app/src/main/java/com/iron/espresso/ext/TextViewExt.kt
+++
@@ -1,7 +0,0 @@
-package com.iron.espresso.ext
-
-import android.widget.TextView
-
-fun TextView.setTextColorIf(defaultColor: Int, changeColor: Int, setDefault: Boolean) {
-    this.setTextColor(if (setDefault) defaultColor else changeColor)
-}
