41560e81735c43b304f8f23ac9ec35df9ee2e2f4
josh <josh@watcha.com>
2021-04-05 00:39:03 +0900

    feat(MyStudyFragment.kt) : 안읽은 알림 갯수 뱃지 달음

--- app/src/main/java/com/iron/espresso/presentation/home/mystudy/MyStudyViewModel.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/mystudy/MyStudyViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import com.iron.espresso.AuthHolder
 import com.iron.espresso.Logger
 import com.iron.espresso.base.BaseViewModel
-import com.iron.espresso.data.model.MyStudyListItem
+import com.iron.espresso.data.model.MyStudyItem
 import com.iron.espresso.domain.repo.StudyRepository
 import com.iron.espresso.ext.networkSchedulers
 import com.iron.espresso.ext.plusAssign
@@ -16,9 +16,13 @@ import retrofit2.HttpException
 class MyStudyViewModel @ViewModelInject constructor(private val studyRepository: StudyRepository) :
     BaseViewModel() {
 
-    private val _studyList = MutableLiveData<MyStudyListItem>()
-    val studyList: LiveData<MyStudyListItem>
+    private val _studyList = MutableLiveData<List<MyStudyItem>>()
+    val studyList: LiveData<List<MyStudyItem>>
         get() = _studyList
+
+    private val _badgeCount = MutableLiveData<Int>()
+    val badgeCount: LiveData<Int>
+        get() = _badgeCount
 
     fun showMyStudyList() {
         compositeDisposable += studyRepository
@@ -30,8 +34,8 @@ class MyStudyViewModel @ViewModelInject constructor(private val studyRepository:
             }
             .networkSchedulers()
             .subscribe({
-                _studyList.value = it
-                Logger.d("$it")
+                _badgeCount.value = it.badge.alert
+                _studyList.value = it.myStudyItem
             }, {
                 val errorResponse = (it as? HttpException)?.toErrorResponse()
                 Logger.d("$errorResponse")
