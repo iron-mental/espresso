685abf97e1c95167a5064359f09c4181acac3c54
josh <josh@watcha.com>
2021-04-04 17:02:41 +0900

    fix(...) : etc호출빼기, 설정 탭 상단 프로필 정리

--- app/src/main/java/com/iron/espresso/presentation/home/study/StudyFragment.kt
+++ app/src/main/java/com/iron/espresso/presentation/home/study/StudyFragment.kt
@@ -1,30 +1,30 @@
 package com.iron.espresso.presentation.home.study
 
 import android.os.Bundle
-import android.view.*
-import android.widget.ImageView
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.iron.espresso.R
 import com.iron.espresso.base.BaseFragment
 import com.iron.espresso.databinding.FragmentStudyBinding
 import com.iron.espresso.presentation.home.study.adapter.CategoryAdapter
-import com.iron.espresso.presentation.home.study.adapter.viewholder.StudyCategoryAdapterListener
 import com.iron.espresso.presentation.home.study.list.StudyListActivity
 import com.iron.espresso.presentation.home.study.search.SearchStudyActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StudyFragment :
-    BaseFragment<FragmentStudyBinding>(R.layout.fragment_study),
-    StudyCategoryAdapterListener {
+    BaseFragment<FragmentStudyBinding>(R.layout.fragment_study) {
 
-    private val categoryAdapter by lazy { CategoryAdapter() }
-    private val viewModel by viewModels<StudyViewModel>()
-
-    override fun getData(item: String, imageView: ImageView) {
-        startActivity(StudyListActivity.getIntent(requireContext(), item))
+    private val categoryAdapter by lazy {
+        CategoryAdapter { item ->
+            startActivity(StudyListActivity.getIntent(requireContext(), item))
+        }
     }
+    private val viewModel by viewModels<StudyViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -33,7 +33,6 @@ class StudyFragment :
             viewStudyCategory.adapter = categoryAdapter
             viewStudyCategory.layoutManager = GridLayoutManager(context, SPAN_COUNT)
         }
-        categoryAdapter.setItemClickListener(this)
 
         viewModel.getStudyCategory()
         viewModel.categoryList.observe(viewLifecycleOwner, { categoryList ->
